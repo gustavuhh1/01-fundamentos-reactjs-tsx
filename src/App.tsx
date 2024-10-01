13d8d6cc0da2e9d1b7e9aa3a1554281dada99c09
Gustavo Martins Rodrigues <gustavomrodrigues11@gmail.com>
2024-10-01 15:33:12 -0300

    start: Continuidade do projeto reactjs em TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+// JSX = Javascript + XML (HTML)
+import { Post } from "./components/Post.js";
+import { Header } from "./components/Header.js";
+import { Sidebar } from "./components/Sidebar.js";
+import styles from './App.module.css';
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/gustavuhh1.png',
+      name: 'Gustavo Martins',
+      role: 'Estágiario TI'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa, O salomão foi promovido a Analista de TI e so faz merda', },
+      { type: 'paragraph', content: 'Hoje eu to desenvolvendo em ReactJs, Muito foda!!', },
+      { type: 'link', content: 'github.com/gustavuhh1', },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/elishowbr.png',
+      name: 'Elisson Victor',
+      role: 'Monitor Dev'
+    },
+    content: [
+      { type: 'paragraph', content: 'TUMANU CUN MAN, SI FUDER SOU ESCRIVÃO DE MERDA', },
+      { type: 'paragraph', content: 'Fala will, vai te fuder vou dar essa aula não!! vlw vlw', },
+      { type: 'link', content: 'github.com/elishowbr', },
+    ],
+    publishedAt: new Date('2024-09-12 20:00:00'),
+  }
+]
+
+
+
+export function App() {
+
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+            <Post
+            key={post.id}
+            author={post.author}
+            content={post.content}
+            publishedAt={post.publishedAt}
+            />
+          )
+
+
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
+
+
---
+++ src/components/Post.tsx
@@ -0,0 +1,126 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
+import { Comment } from './Comment'
+import style from './Post.module.css'
+import { Avatar } from './Avatar'
+import { useState } from 'react';
+
+interface Author {
+    name: string;
+    role: string;
+    avatarUrl: string;
+}
+
+interface PostProps{
+    author: Author;
+    publishedAt: Date;
+    content: string
+}
+
+export function Post({ author, publishedAt, content} :PostProps) {
+    const [comments, setComments] = useState([
+        "Post muito bacana, heim?!"
+    ])
+
+    const [newCommentText, setNewCommentText] = useState('')
+
+
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    })
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR, 
+        addSuffix: true,
+
+    })
+
+    function handleCreateNewComment() {
+        event.preventDefault()
+
+
+        setComments([...comments, newCommentText]);  
+        
+        setNewCommentText("");
+    }
+
+    function handleNewCommentChange() {
+        event.target.setCustomValidity('');
+        setNewCommentText(event.target.value);
+        
+        
+    }
+    function handleNewCommentInvalid(){
+        event.target.setCustomValidity('Esse campo é obrigatório!');
+        
+    }
+
+    function deleteComment(commentTOdelete){
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment !== commentTOdelete
+        })
+
+        setComments(commentsWithoutDeletedOne);
+    }
+
+    const isNewCommentEmpty = newCommentText.length === 0
+
+    return (
+        <article className={style.post}>
+            <header>
+                <div className={style.author}>
+                    <Avatar hasBorder src={author.avatarUrl}/>
+                 
+                    <div className={style.authorInfo}>
+                    <strong>{author.name}</strong>
+                    <span>{author.role}</span>
+                    </div>
+                </div>
+
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
+            </header>
+
+            <div className={style.content}>
+                {content.map(line => {
+                    if (line.type === 'paragraph') {
+
+                        return <p key={line.content}>{line.content}</p>;
+                    } else if (line.type === 'link') {
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
+                    }})
+                }
+            </div>
+            <form onSubmit={handleCreateNewComment} className={style.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea
+                    name='comment'
+                    placeholder='Deixe um comentário'
+                    onChange={handleNewCommentChange}
+                    value={newCommentText}
+                    onInvalid={handleNewCommentInvalid}
+                    required
+                />
+
+                <footer>
+                    <button 
+                    type='submit' 
+                        disabled={isNewCommentEmpty}
+                    >Publicar</button>
+                </footer>
+                
+            </form>
+
+            <div className={style.commentList}>
+                {comments.map(comment => {
+                    return (<Comment 
+                        key={comment} 
+                        content={comment} 
+                        OnDeleteComment={deleteComment}
+                    />
+                    )   
+                })}
+            </div>
+        </article>
+    )
+}
